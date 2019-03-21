7ac847897446a1c3465ddb28fadd71fca5761233
Chandra Shekhar Yadav <shekharmak10@gmail.com>
2019-03-21 12:21:19 +0530

    Update Division.java

--- src/main/java/com/shekhar/expressioncalculator/Division.java
+++ src/main/java/com/shekhar/expressioncalculator/Division.java
@@ -1,16 +1,17 @@
 package com.shekhar.expressioncalculator;
 
+import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.Map;
 
 class Division extends Expression{
-
+    private static final MathContext MATH_CONTEXT = new MathContext(2, RoundingMode.HALF_UP);
     Division(Expression left, Operator operator, Expression right) {
         super(left, operator, right);
     }
 
     @Override
     void setData(Map<String, Object> data) {
-        this.data = left.data.divide(right.data, RoundingMode.HALF_UP);
+        this.data = left.data.divide(right.data, MATH_CONTEXT);
     }
 }
